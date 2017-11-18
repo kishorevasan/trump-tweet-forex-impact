77ecbf95f5a83cdc2edfafbc9a6c9fdd466a48c6
MyungJin Eun <eunmyun@uw.edu>
2017-11-17 17:53:09 -0800

    ttest with control group

--- scripts/step2.R
+++ scripts/step2.R
@@ -2,22 +2,41 @@ library(dplyr)
 tweets <- read.csv(file='../data/unique_dates.csv', stringsAsFactors = FALSE)
 currency <- read.csv(file='../data/kor_currency_cleaned.csv', stringsAsFactors = FALSE)
 
-colnames(currency) <- c('date', 'currency')
-
 currency$date <- as.Date(currency$date)
 tweets$created_at <- as.Date(tweets$created_at)
 
 delta1 <- c()
 delta2 <- c()
-curr <- currency %>%
-        filter(date == tweets$created_at[1]) %>%
-        select(date)
+
+delta.rand1 <- c()
+delta.rand2 <- c()
+
+start.date <- tweets$created_at[1]
+end.date <- tweets$created_at[length(tweets$created_at)]
+
+dates <- seq(start.date, end.date, by="days")
+
+new.data <- subset(currency, date %in% dates)
+new.data <- new.data[!(new.data$date %in% tweets$created_at),]
+
+for(i in 1:5) {
+  rand <- new.data[sample(nrow(new.data), length(tweets$created_at)), ]
+  for (i in 1:length(rand)) {
+    index <- match(rand$date[i], currency$date)
+    before1 <- as.numeric(currency$currency[index-1])
+    current1 <- as.numeric(currency$currency[index])
+    after1 <- as.numeric(currency$currency[index+1])
+    delta.rand1[i] <- (current1-before1)
+    delta.rand2[i] <- (after1-current1)
+  }
+  delta.rand1 <- unlist(delta.rand1)
+  delta.rand2 <- unlist(delta.rand2)
+  print(t.test(delta.rand1, delta.rand2, paired=TRUE))
+}
 
 for (i in 1:length(tweets$created_at)) {
   idx <- match(tweets$created_at[i], currency$date)
-  # print(idx)
   if (!is.na(idx)) {
-    print(idx)
     before <- as.numeric(currency$currency[idx-1])
     current <- as.numeric(currency$currency[idx])
     after <- as.numeric(currency$currency[idx+1])
@@ -29,4 +48,3 @@ for (i in 1:length(tweets$created_at)) {
 delta1 <- unlist(delta1)
 delta2 <- unlist(delta2)
 t.test(delta1, delta2, paired=TRUE)
-
